ad98b919e5f5f8a5027db09a915d5ca0e4c371bf
Hakan Nilsson <haakan@gmail.com>
2022-11-02 22:49:41 +0100

    Try now

--- apps/els_lsp/priv/code_navigation/src/diagnostics_xref_types.erl
+++ apps/els_lsp/priv/code_navigation/src/diagnostics_xref_types.erl
@@ -6,6 +6,6 @@
 
 -type type_a() :: code_navigation:non_existing().
 
--spec function_a(code_navigation:type_a()) -> code_navigation:non_existing().
+-spec function_a(orddict:orddict()) -> code_navigation:non_existing().
 function_a(_) ->
     ok.
